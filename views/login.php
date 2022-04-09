10a7b9a733de61369abb53559ef6c330b84a0253
kaamonchanokk <91516215+kaamonchanokk@users.noreply.github.com>
2022-04-09 23:38:58 +0700

    update login2

--- views/login.php
+++ views/login.php
@@ -9,6 +9,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
 </head>
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Sarabun&display=swap');
