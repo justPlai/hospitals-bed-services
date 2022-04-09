80ad0840ef18ee40343f4a16d1f013d87df36673
ArtidYenpram <plaiblindspot@gmail.com>
2022-04-10 01:41:03 +0700

    update navbar and index

--- views/index.php
+++ views/index.php
@@ -1,3 +1,8 @@
+<style>
+    #search {
+        margin-left: 200%;
+    }
+</style>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,11 +12,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hospital Bed Services</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-        crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <!-- ajax -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <!-- Popper JS -->
@@ -20,8 +22,18 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
     <!-- <script src="sweetalert2.all.min.js"></script> -->
-    
+
 </head>
-    <?php 
-        require_once("./navbar.php")
-    ?>
+<?php
+require_once("./navbar.php")
+?>
+
+<body>
+    <div id="search">
+        <form>
+            <input type="text" />
+        </form>
+    </div>
+</body>
+
+</html>
