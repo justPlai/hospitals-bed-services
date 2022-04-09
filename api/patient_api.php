0f41cf20e67ef7f6ead6e8ce6a7bda38a7fad1eb
ArtidYenpram <plaiblindspot@gmail.com>
2022-04-10 04:29:02 +0700

    Create patient_api.php

---
+++ api/patient_api.php
@@ -0,0 +1,66 @@
+<?php
+class hospital_api
+{
+
+    public function __construct()
+    {
+    }
+
+    public static function getAll()
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://soa-project-final.herokuapp.com/api/patients");
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $output = curl_exec($ch);
+        curl_close($ch);
+        $obj = json_decode($output, true);
+        //echo $output;
+        return $obj['body'];
+    }
+
+    public static function getById($id)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://soa-project-final.herokuapp.com/api/patients/$id");
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $output = curl_exec($ch);
+        curl_close($ch);
+        $obj = json_decode($output, true);
+        //echo $output;
+        return $obj['body'][0];
+    }
+
+    public static function create($firstName, $lastName, $BOD, $IDCard, $createDate)
+    {
+        $url = "https://soa-project-final.herokuapp.com/api/patients";
+        $data = array('firstName' => $firstName, 'lastName' => $lastName, 'BOD' => $BOD, 'IDCard' => $IDCard, 'createDate' => $createDate);
+        $data_json = json_encode($data);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_json);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response  = curl_exec($ch);
+        curl_close($ch);
+        // echo $response;
+    }
+
+    public static function update($_id, $firstName, $lastName, $BOD, $IDCard, $createDate)
+    {
+        $url = "https://soa-project-final.herokuapp.com/api/patients";
+        $data = array('_id' => $_id, 'firstName' => $firstName, 'lastName' => $lastName, 'BOD' => $BOD, 'IDCard' => $IDCard, 'createDate' => $createDate);
+        $data_json = json_encode($data);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Content-Length: ' . strlen($data_json)));
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_json);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response  = curl_exec($ch);
+        curl_close($ch);
+        // echo $response;
+    }
+}
