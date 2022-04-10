23f1ab76ef271bfa8de54b756723c64b6521bb19
kaamonchanokk <91516215+kaamonchanokk@users.noreply.github.com>
2022-04-10 23:46:05 +0700

    Doctor1

---
+++ views/doctor/index_Doctor.php
@@ -0,0 +1,134 @@
+<style>
+    body {
+        font-family: 'Sarabun', sans-serif;
+        overflow: hidden;
+    }
+
+    #searchbed {
+        position: relative;
+        right: 40px;
+        width: 500px;
+        font-size: large;
+
+    }
+</style>
+
+<body>
+
+    <div class="container mt-5">
+        <div class="row" style="margin-bottom: 20px;">
+            <div class="col-1">
+                <i class="bi bi-hospital-fill" style='font-size:50px; top: 15px; position:relative;'></i>
+            </div>
+            <div class="col mt-4">
+                <input type="text" id="searchbed" onkeyup="myFunction()" placeholder="ค้นหา...">
+
+                <input type="hidden" name="controller" value="hospital">
+                <input type="hidden" name="action" value="index">
+            </div>
+            <div class="col mt-4">
+                <a class="btn btn-primary" href="?controller=bed&action=createPage" style='left: 60%; position:relative;'>เพิ่มเตียงโรงพยาบาล</a><br>
+            </div>
+        </div>
+
+        <table class="table table-hover" id="myTable">
+            <thead class="table-dark text-center">
+                <tr>
+                    <th>ลำดับที่</th>
+                    <th>โรงพยาบาล</th>
+                    <th>ห้อง</th>
+                    <th>แก้ไข</th>
+                    <th>ลบ</th>
+                </tr>
+                <!--รอ service ครบค่อยใส่ foreach-->
+            </thead>
+            <tbody class="text-center">
+                <?php
+                foreach ($bedList as $bedList) {
+                    echo "<tr>
+                            <td>$bedList->bed_id</td>
+                            <td>$bedList->hospital_name</td>
+                            <td>$bedList->room_id</td>
+                            <td><a type=button href=?controller=bed&action=updatePage&id=$bedList->bed_id >📝</a></td>";
+                ?>
+                    <td>
+                        <form method=POST action="?controller=bed&action=delete">
+                            <input name="id" type="hidden" value="<?php echo "$bedList->bed_id"; ?>">
+                            <a type="button" class="show_confirm" data-toggle="tooltip" test="<?php echo $bedList->bed_id; ?>">🚮</a>
+                        </form>
+                    </td>
+                <?php
+                    echo "</tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+
+    </div>
+</body>
+
+</html>
+<script>
+    function myFunction() {
+        var input, filter, table, tr, td, i, txtValue;
+        input = document.getElementById("searchbed");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("myTable");
+        tr = table.getElementsByTagName("tr");
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[1];
+            td2 = tr[i].getElementsByTagName("td")[2];
+            let x = 1;
+            if (td) {
+                txtValue = td.textContent || td.innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                    x = 0;
+                }
+            }
+
+            if (x === 1) {
+                if (td2) {
+                    txtValue = td2.textContent || td2.innerText;
+                    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                        tr[i].style.display = "";
+                    } else {
+                        tr[i].style.display = "none";
+                    }
+                }
+            }
+        }
+
+
+    }
+</script>
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script type="text/javascript">
+    $('.show_confirm').click(function(event) {
+        var form = $(this).closest("form");
+        var name = $(this).data("name");
+        var test = $(this).attr("test");
+        event.preventDefault();
+        Swal.fire({
+            title: 'แน่ใจว่าคุณต้องการลบ?',
+            text: "" + test,
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'ลบ',
+            cancelButtonText: 'ยกเลิก'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire(
+                        'ลบเสร็จสิ้น!',
+                        'เตียงนี้ได้ถูกลบไปแล้ว',
+                        'success'
+                    )
+                    .then(function() {
+                        form.submit(); // <--- submit form programmatically
+                    });
+            }
+        })
+    });
+</script>
---
+++ views/doctor/newDoctor.php
@@ -0,0 +1,106 @@
+<style>
+    html,
+    body {
+        height: 100%;
+    }
+
+    body {
+        margin: 0;
+        background: "white";
+        font-family: 'Sarabun', sans-serif;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+    }
+
+    .block-1 {
+        width: 1000px;
+        height: 500px;
+        background: #FFFFFF;
+        box-shadow: 0 3px 6px rgba(0, 0, 0, 0.75);
+        border-radius: 10px;
+    }
+
+    input[type=text],
+    select,
+    textarea {
+        width: 800px;
+        height: 40px;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        resize: vertical;
+        font-size: 20px;
+    }
+
+    input[type=date] {
+        width: 800px;
+        height: 40px;
+        padding: 12px;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        resize: vertical;
+        font-size: 20px;
+    }
+
+    label {
+        position: relative;
+        font-size: 25px;
+    }
+
+    .row {
+        position: relative;
+        top: 20px;
+    }
+</style>
+
+<body>
+    <center>
+        <div>
+
+            <div style="position: relative; right:320px; margin-top:3%">
+                <h2><i class="fa fa-wrench" style="font-size:50px;" aria-hidden="true"></i>เพิ่มข้อมูลเตียง</h2>
+            </div>
+
+            <div class="block-1">
+                <form method="get" action="">
+
+                    <div>
+
+                        <div class="row">
+                            <div class="col" style="position: relative; margin-top:5%">
+                                <label style="margin-right:65%;">หมายเลขห้อง</label>
+                                <input type="text" class="form-control" name="room_id" /><br>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col" style="position: relative; margin-top:5%">
+                                <label style="margin-right:65%;">โรงพยาบาล</label>
+                                <select name="hospital_id">
+                                    <?php foreach ($hospitalList as $hospitalList) {
+                                        echo "<option value = $hospitalList->hospital_id";
+                                        if ($hospitalList->hospital_id == $bedList->hospital_id) {
+                                            echo " selected='selected'";
+                                        }
+                                        echo ">$hospitalList->hospital_name</option>";
+                                    } ?></select>
+                            </div>
+                        </div>
+
+
+                        <input type="hidden" name="controller" value="bed" />
+                        <!--ใส่ id-->
+                        <div class="row">
+                            <div class="col" style="position: relative; margin-top:10%">
+                                <button class="btn btn-primary" type="submit" name="action" value="create">เพิ่ม</button>
+                                <button class="btn btn-primary" type="submit" name="action" value="index">ย้อนกลับ</button>
+                            </div>
+                        </div>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    </center>
+</body>
+
+</html>
