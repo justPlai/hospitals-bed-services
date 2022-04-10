2d14c9ba02a24430a5e94e021fd5f371c74c05e5
kaamonchanokk <91516215+kaamonchanokk@users.noreply.github.com>
2022-04-11 06:26:47 +0700

    updatemain

--- views/main/home.php
+++ views/main/home.php
@@ -1,13 +1,14 @@
 <style>
     body,
     html {
+        font-family: 'Sarabun', sans-serif;
         height: 100%;
         margin: 0;
     }
 
     .bg {
         /* The image used */
-        background-image: url("img/5.jpg");
+        background-image: url("img/55.jpg");
 
         /* Full height */
         height: 100%;
@@ -23,27 +24,29 @@
     <div class="bg">
 
         <div class="row">
-            <div class="col" style="position: relative; margin-top:5%">
-                <h1 style="margin-right:60%;">10</h1>
+            <div class="col" style="position: relative; margin-top:12%">
+                <h1 style="font-size:80px;margin-left:35%;">100</h1>
             </div>
-            <div class="col" style="position: relative; margin-top:5%">
-                <h1 style="margin-right:60%;">จำนวนผู้ป่วย</h1>
-                <h1 style="margin-right:60%;">ที่รับรักษาที่โรงพยาบาล</h1>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col" style="position: relative; margin-top:5%">
-                <h1 style="margin-right:60%;">100</h1>
-                <h1 style="margin-right:60%;">จำนวนเตียง</h1>
-                <h1 style="margin-right:60%;">คงเหลือทั้งหมด</h1>
+            <div class="col" style="position: relative; margin-top:12%;right:20%;">
+                <h1>จำนวนผู้ป่วย</h1>
+                <h1 >ที่รับรักษาที่โรงพยาบาล</h1>
             </div>
         </div>
 
         <div class="row">
             <div class="col" style="position: relative; margin-top:5%">
-                <h1 style="margin-right:60%;">โรงพยาบาลชลประทาน</h1>
-                <h1 style="margin-right:60%;">มีจำนวนผู้เข้ารักษามากที่สุด</h1>
+                <h1 style="font-size:80px;margin-left:35%;">100</h1>
+            </div>
+            <div class="col" style="position: relative; margin-top:5%;right:20%;">
+                <h1 >จำนวนเตียง</h1>
+                <h1 >คงเหลือทั้งหมด</h1>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col" style="position: relative; margin-top:5%;left:18%;">
+                <h1 >โรงพยาบาลชลประทาน</h1>
+                <h1 >มีจำนวนผู้เข้ารักษามากที่สุด</h1>
             </div>
         </div>
 
