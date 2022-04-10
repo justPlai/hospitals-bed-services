6e701206254ccf5d189cb12e62afdff0cb8c6a3e
kaamonchanokk <91516215+kaamonchanokk@users.noreply.github.com>
2022-04-10 12:30:14 +0700

    update detail model

--- controllers/pages_controller.php
+++ controllers/pages_controller.php
@@ -3,8 +3,8 @@ class PagesController
 {
     public function home()
     { 
-        $k= bed::getAll();
-        echo $k[0]->room_id;
+        //hospital::add("ฉัน","คือ","คนไทย");
+        //echo $k[0]->room_id;
         require_once('index.php');
     }
     public function error()
