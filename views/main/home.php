1e2e2199f8addac29f82800649f7103e302490b2
kaamonchanokk <91516215+kaamonchanokk@users.noreply.github.com>
2022-04-11 06:05:48 +0700

    home

--- views/main/home.php
+++ views/main/home.php
@@ -1,24 +1,54 @@
 <style>
-body, html {
-  height: 100%;
-  margin: 0;
-}
+    body,
+    html {
+        height: 100%;
+        margin: 0;
+    }
 
-.bg {
-  /* The image used */
-  background-image: url("img/3.jpg");
+    .bg {
+        /* The image used */
+        background-image: url("img/5.jpg");
 
-  /* Full height */
-  height: 100%; 
+        /* Full height */
+        height: 100%;
 
-  /* Center and scale the image nicely */
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-}
+        /* Center and scale the image nicely */
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
 </style>
+
 <body>
-    <div class="bg"></div>
+    <div class="bg">
+
+        <div class="row">
+            <div class="col" style="position: relative; margin-top:5%">
+                <h1 style="margin-right:60%;">10</h1>
+            </div>
+            <div class="col" style="position: relative; margin-top:5%">
+                <h1 style="margin-right:60%;">จำนวนผู้ป่วย</h1>
+                <h1 style="margin-right:60%;">ที่รับรักษาที่โรงพยาบาล</h1>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col" style="position: relative; margin-top:5%">
+                <h1 style="margin-right:60%;">100</h1>
+                <h1 style="margin-right:60%;">จำนวนเตียง</h1>
+                <h1 style="margin-right:60%;">คงเหลือทั้งหมด</h1>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col" style="position: relative; margin-top:5%">
+                <h1 style="margin-right:60%;">โรงพยาบาลชลประทาน</h1>
+                <h1 style="margin-right:60%;">มีจำนวนผู้เข้ารักษามากที่สุด</h1>
+            </div>
+        </div>
+
+    </div>
+
 </body>
 
 </html>
