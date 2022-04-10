41bd94dc11d701231072ce9ad256ab66b34d5c28
ArtidYenpram <plaiblindspot@gmail.com>
2022-04-10 21:54:33 +0700

    update beddetail

--- controllers/bedDetail_controller.php
+++ controllers/bedDetail_controller.php
@@ -3,8 +3,8 @@ class BedDetailController
 {
     public function index()
     {
-        $id = $_GET['id'];
-        $bedDetailList = bedDetail::getByHospitalId($id);
+        $hospital_id = $_GET['hospital_id'];
+        $bedDetailList = bedDetail::getByHospitalId($hospital_id);
         require_once('views/detailbed/index_Detailbed.php');
     }
     public function updatePage()
@@ -23,10 +23,9 @@ class BedDetailController
         $patient_id = $_GET['patient_id'];
         $bed_id = $_GET['bed_id'];
         $doctor_id = $_GET['doctor_id'];
-        echo "$bedDetail_id, $patient_id, $bed_id, $doctor_id";
         bedDetail::update($bedDetail_id, $patient_id, $bed_id, $doctor_id);
 
-        $id = $_GET['hospital_id'];
+        $hospital_id = $_GET['hospital_id'];
         BedDetailController::index();
     }
 }
